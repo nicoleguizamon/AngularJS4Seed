427367aa9deb681ebcd4a9c4004903565c34186d
ramiro <ramiro.rigattieri@glubatec.com>
2017-08-28 12:35:04 -0300

    Fixed download error

--- src/app/services/download-file.service.ts
+++ src/app/services/download-file.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions, RequestMethod, ResponseContentType } from '@angular/http';
-//import 'rxjs/Rx';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DownloadFileService {
@@ -11,19 +11,32 @@ export class DownloadFileService {
   }
 
   download() { //get file from service
-    return this.http.post(this.getUrl('User/DownloadFile'), JSON.stringify({id:1}), this.getOptions()).subscribe(data => this.downloadFile(data)),
-                              error => console.log("Error downloading the file.");
+    return this.http.post(this.getUrl('User/DownloadFile'), JSON.stringify({id:1}), this.getOptions())
+                .subscribe(data => this.downloadFile(data)),
+                    error => console.log("Error downloading the file.");
   }
 
   downloadFile(data: Response) {
-    var blob = new Blob([data], { type: 'text/csv' });//types: 'application/pdf' 'text/csv' 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    var url= window.URL.createObjectURL(blob);
-    window.open(url);
+    var blob = new Blob([data.arrayBuffer()], { type: 'application/pdf' }); //types: 'application/pdf' 'text/csv' 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    var url = window.URL.createObjectURL(blob);
+
+    if(navigator.msSaveOrOpenBlob) {
+      navigator.msSaveBlob(blob, 'Book.csv');
+    } else {
+      let a = document.createElement('a');
+      a.href = url;
+      a.download = 'Book.pdf';
+      document.body.appendChild(a);
+      a.click();        
+      document.body.removeChild(a);
+    }
+    window.URL.revokeObjectURL(url);
+    //window.open(url);
   }
 
   private getOptions(): RequestOptions {
     let auth = new Headers({'Authorization': 'Bearer ' + localStorage.getItem('token')});
-    let options = new RequestOptions({ headers: auth });//method: RequestMethod.Post,
+    let options = new RequestOptions({ headers: auth, responseType: ResponseContentType.ArrayBuffer });//method: RequestMethod.Post,
     return options;
   }
 
