e7688830bbaca73fd5dd50c45095a4224c2c7f0d
ramiro <ramiro.rigattieri@glubatec.com>
2017-08-29 12:25:07 -0300

    Add class BaseService

---
+++ src/app/services/base-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Headers, RequestOptions, ResponseContentType } from '@angular/http';
+
+@Injectable()
+export class BaseService {
+  //private apiUrl = 'http://portalsigic.glubatec.com/api/';
+  private apiUrl = 'http://localhost:39048/api/';
+
+  constructor() { }
+
+  public getUrl(modelo:string)
+  {
+      return this.apiUrl + modelo;
+  }
+
+  public getOptions(responseType?: ResponseContentType): RequestOptions {
+    let auth = new Headers({'Authorization': 'Bearer ' + localStorage.getItem('token')});
+    let options = new RequestOptions({ headers: auth });
+    if(responseType != null) {
+      options.responseType = responseType; 
+    }
+
+    return options;
+  }
+}
